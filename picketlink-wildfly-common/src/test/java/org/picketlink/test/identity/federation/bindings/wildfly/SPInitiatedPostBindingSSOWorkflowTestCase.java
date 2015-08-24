0da9fa7804ffcd300af9c6711bcacc264832b925
Peter Skopek <pskopek@redhat.com>
2015-08-24 20:38:43 +0200

    Fixing SPInitiatedPostBindingSSOWorkflowTestCase.

--- picketlink-wildfly-common/src/test/java/org/picketlink/test/identity/federation/bindings/wildfly/SPInitiatedPostBindingSSOWorkflowTestCase.java
+++ picketlink-wildfly-common/src/test/java/org/picketlink/test/identity/federation/bindings/wildfly/SPInitiatedPostBindingSSOWorkflowTestCase.java
@@ -17,24 +17,7 @@
  */
 package org.picketlink.test.identity.federation.bindings.wildfly;
 
-import io.undertow.server.handlers.resource.ResourceManager;
-import io.undertow.servlet.api.DeploymentInfo;
-import io.undertow.servlet.api.DeploymentManager;
-import io.undertow.servlet.api.InstanceFactory;
-import io.undertow.servlet.api.InstanceHandle;
-import io.undertow.servlet.api.ListenerInfo;
-import io.undertow.servlet.api.LoginConfig;
-import io.undertow.servlet.api.ServletContainer;
-import io.undertow.servlet.api.ServletInfo;
-import io.undertow.servlet.api.ServletSecurityInfo;
-import org.junit.Before;
 import org.junit.Test;
-import org.picketlink.identity.federation.bindings.wildfly.sp.SPFormAuthenticationMechanism;
-
-import javax.servlet.ServletException;
-import java.util.EventListener;
-
-import static junit.framework.Assert.assertNotNull;
 
 /**
  * Unit test the SP Initiated Workflow with HTTP/POST Binding
